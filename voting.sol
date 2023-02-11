91a6834046fd0a0a2c8ff30fd94490fd75cf44cb
Guilhain Averlant <g.averlant@mailfence.com>
2023-02-11 17:56:18 +0100

    add all function

--- voting.sol
+++ voting.sol
@@ -8,13 +8,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
 
-
-    address[] votersIdArray;
     mapping(address => Voter) voters;
+    mapping(uint => address[]) votersDistribution;
     uint public nbProposal;
     mapping (uint => Proposal) public proposals;
 
-    uint nb_voting;
+    uint nbVoting;
 
     struct Voter {
         bool isRegistered;
@@ -36,31 +35,49 @@ contract Voting is Ownable {
         VotesTallied
     }
 
-
-
     event VoterRegistered(address voterAddress); 
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
     event ProposalRegistered(uint proposalId);
     event Voted (address voter, uint proposalId);
 
-    modifier onlyVoter() {
-        require(voters[msg.sender].isRegistered, "only registered voter can summit proposal");
+    modifier onlyOwnerOrVoter()  {
+        _checkOwnerOrVoter();
         _;
     }
-    /*
-    // QUestion
-    Est ce qu'un votant peut soumettre plusieurs propositions ? (pour le moment oui)
-    
-    // BONUS
-    function voterAdd() onlyOwner; // interdit pendant la phase de vote
-    function voterRevoke() onlyOwner; // interdit pendant la phase de vote
-    function delegateMyVote();  // interdit pendant la phase de vote
+        
+    modifier onlyVoter() {
+        _checkVoter();
+        _;
+    }
 
+    modifier voteValidator(uint _proposalId) {
+        _checkAlreadyVote();
+        _checkProposalId(_proposalId);
+        uint beforeVoteProposalCount = proposals[_proposalId].voteCount;
+        _;
+        assert(beforeVoteProposalCount + 1 == proposals[_proposalId].voteCount);
+        assert(voters[msg.sender].hasVoted);
+        assert(votersDistribution[_proposalId].length == beforeVoteProposalCount + 1);
+    }
 
-    */
+    function _checkOwnerOrVoter() internal view virtual {
+        require(voters[msg.sender].isRegistered || msg.sender == owner(), "only owner of this contract and voters can access to this function.");
+    }
+
+    function _checkVoter()  internal view virtual {
+        require(voters[msg.sender].isRegistered, "only registered voter can summit proposal.");
+
+    }
+
+    function _checkAlreadyVote() internal view virtual {
+        require(voters[msg.sender].hasVoted == false, "you have already voted");
+    }
+
+    function _checkProposalId(uint _proposalId) internal view {
+        require(_proposalId > 0 && _proposalId < nbProposal+1, string.concat("the identifier of Proposal does not exist. Please indicate a value between 1 and ", Strings.toString(nbProposal)));
+    }
 
     function addVoter(address _address) public onlyOwner {
-        votersIdArray.push(_address);
         voters[_address] = Voter(true, false, 0);
     }
 
@@ -78,28 +95,37 @@ contract Voting is Ownable {
 
 
     function getProposalByIndex(uint _proposalId) public view onlyVoter returns(Proposal memory) {
-        require(_proposalId > 0 && _proposalId < nbProposal+1, string.concat("the identifier of Proposal does not exist. Please indicate a value between 1 and ", Strings.toString(nbProposal)));
+        _checkProposalId(_proposalId);
         return proposals[_proposalId];
     }
 
-    function vote(uint _proposalId) external onlyVoter {
-        require(voters[msg.sender].hasVoted == false, "you have already voted");
-        require(_proposalId > 0 && _proposalId < nbProposal+1, string.concat("the identifier of Proposal does not exist. Please indicate a value between 1 and ", Strings.toString(nbProposal)));
-        uint beforeVoteProposalCount = proposals[_proposalId].voteCount;
+    function vote(uint _proposalId) external onlyVoter voteValidator(_proposalId) {
         voters[msg.sender].votedProposalId = _proposalId;
         proposals[_proposalId].voteCount ++;
-        nb_voting ++;
+        votersDistribution[_proposalId].push(msg.sender);
+        nbVoting ++;
         voters[msg.sender].hasVoted = true;
-        assert(beforeVoteProposalCount + 1 == proposals[_proposalId].voteCount);
-        assert(voters[msg.sender].hasVoted);
     }
 
+    function didIVote() external view onlyVoter returns(bool) {
+        return voters[msg.sender].hasVoted;
+    }
 
-    function getProposalWinner() external view onlyVoter returns(uint) {
+    function whichProposalDidIVote() external view onlyVoter returns(uint) {
+        require(voters[msg.sender].hasVoted, "you did not vote");
+        return voters[msg.sender].votedProposalId;
+
+    }
+    
+    function voteParticipation() external view onlyOwnerOrVoter returns(uint) {
+        return nbVoting;
+    }
+
+    function getProposalWinner() external view onlyOwnerOrVoter returns(uint) {
         uint maxVote;
         uint maxVoteProposalId;
         bool equality;
-        for (uint proposalId = 0; proposalId < nbProposal; proposalId++) {
+        for (uint proposalId = 1; proposalId < nbProposal + 1; proposalId++) {
             if (proposals[proposalId].voteCount > maxVote) {
                 maxVote = proposals[proposalId].voteCount;
                 maxVoteProposalId = proposalId;
@@ -110,7 +136,16 @@ contract Voting is Ownable {
             else if (proposals[proposalId].voteCount == maxVote) {
                 equality = true;
             }
+            if (equality) {
+                maxVoteProposalId = 0; // no winner
+            }
         }
         return maxVoteProposalId;
     }
+
+    function whoVoteForProposalId(uint _proposalId) external view onlyOwnerOrVoter returns(address[] memory) {
+        _checkProposalId(_proposalId);
+        return votersDistribution[_proposalId];
+    }
+
 }
